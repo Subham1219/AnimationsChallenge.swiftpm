ad2b3609b6ff8fae0a263cfc1fc7184f18f14659
Subham <spathak5540@stu.d214.org>
2023-09-29 10:37:32 -0500

    MVP 1 and 2 done

--- SpinningView1.swift
+++ SpinningView1.swift
@@ -2,26 +2,20 @@ import SwiftUI
 
 struct SpinningView1: View {
     
-    @State var currentDegree = 0.0
+    @State private var currentDegree = 0.0
     
     var body: some View {
         Button("Spins") {
-            //MARK: MVP - Part I
-            
-            
-            
-            
-            
+            withAnimation(.easeIn(duration: 1.0)) {
+                // MARK: MVP - Part I
+                self.currentDegree += 360 // Rotate one time
+            }
         }
         .frame(width: 100, height: 50)
         .background(.blue)
         .foregroundColor(.white)
         .clipShape(RoundedRectangle(cornerRadius: 10.0))
-        //MARK: MVP - Part II
-        
-        
-        
-        
-        
+        // MARK: MVP - Part II
+        .rotationEffect(.degrees(currentDegree))
     }
 }
